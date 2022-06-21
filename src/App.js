a6abccac3b0726ee28957a083a925dd8a4e6aad4
Marcus Guimarães <mrenanrguimaraes@gmail.com>
2022-06-21 15:30:35 -0300

    Correções do lint pt2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-import Title from './components/Title';
 import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
