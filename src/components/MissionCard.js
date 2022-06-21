abd8ab8e4d27ad53ac03f9ab8d95673aeea1ff47
Marcus Guimarães <mrenanrguimaraes@gmail.com>
2022-06-21 16:36:31 -0300

    Requisitos 9 e 10 completos!

---
+++ src/components/MissionCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MissionCard extends React.Component {
+  render() {
+    const { name, year, country, destination } = this.props;
+    return (
+      <div data-testid="mission-card" className="card">
+        <h4 data-testid="mission-name">{ name }</h4>
+        <h4 data-testid="mission-year">{ year }</h4>
+        <h4 data-testid="mission-country">{ country }</h4>
+        <h4 data-testid="mission-destination">{ destination }</h4>
+      </div>
+    );
+  }
+}
+
+MissionCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  destination: PropTypes.string.isRequired,
+};
+
+export default MissionCard;
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        { missions.map(({ name, year, country, destination }) => (
+          <MissionCard
+            key={ name }
+            name={ name }
+            year={ year }
+            country={ country }
+            destination={ destination }
+          />
+        )) }
       </div>
     );
   }
 }
-
 export default Missions;
