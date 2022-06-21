5f556d9dda937aec9c0972fa09cea5ede5ba02b1
Marcus Guimarães <mrenanrguimaraes@gmail.com>
2022-06-21 16:10:59 -0300

    Requisito 6 completo!

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -7,7 +7,7 @@ class PlanetCard extends React.Component {
     return (
       <div data-testid="planet-card">
         <h3 data-testid="planet-name">{ planetName }</h3>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <img src={ planetImage } className="planetImg" alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import planets from '../data/planets';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        { planets.map(({ name, image }) => (
+          <PlanetCard key={ name } planetName={ name } planetImage={ image } />))}
       </div>
     );
   }
